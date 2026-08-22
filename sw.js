a50100ae7dcc93a8754b2ce9981e71ee91c5dbd4
hareharenohara <hareharenohara@gmail.com>
2026-08-22 17:31:22 +0900

    Add daily return goal meter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "race-lab-shell-v18";
+const CACHE_NAME = "race-lab-shell-v19";
 const SHELL = [
   "/index.html", "/styles.css", "/ui.css", "/analytics-core.js", "/app.js",
   "/manifest.webmanifest", "/icons/icon.svg", "/icons/icon-192.png",
