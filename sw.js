60bbfedac8b0157a0a202f16735b976176d3f12a
hareharenohara <hareharenohara@gmail.com>
2026-08-23 19:24:26 +0900

    Keep expanded finish order horizontal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "race-lab-shell-v33";
+const CACHE_NAME = "race-lab-shell-v34";
 const SHELL = [
   "/index.html", "/styles.css", "/ui.css", "/analytics-core.js", "/app.js",
   "/manifest.webmanifest", "/icons/icon.svg", "/icons/icon-192.png",
